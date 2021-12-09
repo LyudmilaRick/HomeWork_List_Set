dc230b2f3af4a16dd7111c3f72b31da6a9b72809
RickLyudmila <Lrick@yandex.ru>
2021-12-09 20:19:12 +0300

    Initial commit

---
+++ src/main/java/pro/sky/java/course2/homework_list_set/HomeWorkListSetApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.homework_list_set;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWorkListSetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeWorkListSetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/java/course2/homework_list_set/HomeWorkListSetApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.homework_list_set;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWorkListSetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
